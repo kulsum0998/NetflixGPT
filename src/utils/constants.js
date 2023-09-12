f31c4b9c7e0708adaed77d28849f4a9a460a0dc4
kulsum0998 <83638819+kulsum0998@users.noreply.github.com>
2023-09-12 13:48:37 +0530

    netflix GPT added

---
+++ src/utils/constants.js
@@ -0,0 +1,25 @@
+export const API_OPTIONS = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MDI0Yzg2Njc5ZDc3Mzg5N2NmMTlkMzRiMTBkOTEwOSIsInN1YiI6IjY0NzE4ZGM5OWFlNjEzMDE0NjY3OGM0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.h2NBYxvdyGp4pGiUd7F5s0gFlWSvOohl0Q_yY74808k",
+    },
+  };
+  
+  export const IMG_CDN = "https://image.tmdb.org/t/p/w500/";
+  
+  export const SUPPORTED_LANGUAGES = [
+    {
+      identifier: "1",
+      name: "English",
+    },
+    {
+      identifier: "2",
+      name: "Hindi",
+    },
+    {
+      identifier: "3",
+      name: "Spanish",
+    },
+  ];
